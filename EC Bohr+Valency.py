1ad698a2ed8b59f85b90fdb322cabf4fc11afa00
Aman07844 <07844@bpsdoha.edu.qa>
2021-09-29 09:53:18 +0300

    added finding valency

--- EC Bohr+Valency.py
+++ EC Bohr+Valency.py
@@ -26,3 +26,28 @@ while EC_var > 0:
 
 
 print(K, L, M, N)
+
+'''
+valency
+'''
+valency = 0
+valence_e = 0
+
+# setting no of valence electrons
+if N > 0:
+    # no of valence electrons is no of electrons is N
+    valence_e = N
+elif M > 0:
+    valence_e = M
+elif L > 0:
+    valence_e = L
+elif K > 0:
+    valence_e = K
+
+# finding valency
+if valence_e <= 4:
+    valency = valence_e
+elif valence_e > 4 and valence_e < 9:
+    valency = 8 - valence_e
+
+print(valency)
